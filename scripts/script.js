cbb2738059bcf15bd2c9ee0707e6f85b841bb736
Lord1nho <luizfernandosv85@gmail.com>
2024-08-06 22:55:59 -0300

    criando tabelaTop30-V1

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-// Requisições iniciais
+// FUNÇÃO BÁSICA PARA BUSCAR PAÍSES DAS OLIMPÍADAS
 async function getOlympicRankByName() {
     try {
         const response = await fetch('https://apis.codante.io/olympic-games/countries')
         const orders = await response.json()
         let name_country = []
         for (let i = 0; i<31; i++){
-            name_country.push(orders.data[i].name);
+            name_country.push(orders.data[i]);
         } 
         return name_country;    
     } catch (error) {
@@ -14,8 +14,32 @@ async function getOlympicRankByName() {
 }
 
 
+
+// async para pegar os países e criar a tabela ao carregar a página.
 async function getCountries() {
     let top30 = await getOlympicRankByName();
-    console.log(top30);
+    let teste = document.querySelector('#medal_table');
+    let line = ''
+    for (let i = 0; i < top30.length; i++) {
+        if (top30[i].name == "Brasil") {
+            line += `<tr class="brasil_position"><td>${top30[i].name}</td>
+        <td>${top30[i].gold_medals}</td>
+        <td>${top30[i].silver_medals}</td>
+        <td>${top30[i].bronze_medals}</td>
+        <td>${top30[i].total_medals}</td>
+        </tr>`;
+        }
+        line += `<tr><td>${top30[i].name}</td>
+        <td>${top30[i].gold_medals}</td>
+        <td>${top30[i].silver_medals}</td>
+        <td>${top30[i].bronze_medals}</td>
+        <td>${top30[i].total_medals}</td>
+        </tr>`;
+    }    
+
+    teste.innerHTML += line;
+    console.log(teste);
 }
+
+getCountries();
 
