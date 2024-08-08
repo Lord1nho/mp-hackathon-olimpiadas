5672ee2a435a2f934019f6cb3997533f630b5264
Lord1nho <luizfernandosv85@gmail.com>
2024-08-07 23:59:14 -0300

    alteração mínima na range da busca de países

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ async function getOlympicRankByName() {
         const response = await fetch('https://apis.codante.io/olympic-games/countries')
         const orders = await response.json()
         let name_country = []
-        for (let i = 0; i<31; i++){
+        for (let i = 0; i<30; i++){
             name_country.push(orders.data[i]);
         } 
         return name_country;    
